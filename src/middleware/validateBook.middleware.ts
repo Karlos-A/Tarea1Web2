c6fe10d614db70f4642d50d8c720b6075487eccb
Karlos_Acosta <karlos.acosta@iteso.mx>
2026-09-02 21:47:21 -0600

    Readme and validateBook updated

--- src/middleware/validateBook.middleware.ts
+++ src/middleware/validateBook.middleware.ts
@@ -2,8 +2,25 @@ import type { Request, Response, NextFunction } from "express";
 
 export const validateBook = (req: Request, res: Response, next: NextFunction) => {
     const { title, author, year } = req.body;
-    if(!title || !author || !year) {
-        return res.status(400).json({ message: "Title, author, and year are required" });
-    }
+    if (!title || typeof title !== "string" || title.trim() === "") {
+    return res.status(400).json({ 
+      error: true, 
+      message: "Title is required and cannot be empty or only spaces" 
+    });
+  }
+
+  if (!author || typeof author !== "string" || author.trim() === "") {
+    return res.status(400).json({ 
+      error: true, 
+      message: "Author is required and cannot be empty" 
+    });
+  }
+
+  if (year === undefined || typeof year !== "number") {
+    return res.status(400).json({ 
+      error: true, 
+      message: "Year is required and must be a valid number" 
+    });
+  }
     next();
 }
